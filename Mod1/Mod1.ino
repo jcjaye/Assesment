2ab06c45e2474daa1f35b69227d3e1f272c13f7a
jcjaye <158523914+jcjaye@users.noreply.github.com>
2024-03-22 13:05:36 +1300

    Update Mod1.ino

--- Mod1/Mod1.ino
+++ Mod1/Mod1.ino
@@ -1,23 +1,33 @@
-const int wire1 = 1;
-
+const byte wire1 = 1;
+const byte wire2 = 2;
+const byte wire3 = 3;
 voide setup() {
 
-Serial.begin(01);
+Serial.begin(9600);
 pinMode(wire1, INPUT);
+pinMode(wire2, INPUT);
+pinMode(wire3, INPUT);
 }
 
 Void loop() {
 
   int wirestate = digitalRead(wire1);
-  Serial.printIn(wirestate);
-  delay(1);
+  Serial.println(wirestate);
+  
 
   //if the wire is cut the wirestate is HIGH
 
-loop () {
-  if (digitalRead(wired) == HIGH) {
+
+  if (digitalRead(wire1) == LOW) {
     //send lose signal"
-    "Serial println("lose");
-    
+    Serial.println("lose");
+  }
+    if (digitalRead(wire2) == LOW) {
+    //send win signal"
+    Serial.println("win");
+  }
+    if (digitalRead(wire3) == LOW) {
+    //send lose signal"
+    Serial.println("lose");
   }
 }
